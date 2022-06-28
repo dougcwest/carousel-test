66ac868afb5f9af6e0bcabdbc004f2f07b6f9bc4
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-06-27 19:47:57 -0500

    description and link

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,35 +3,49 @@ import { images } from './helpers/CarouselData';
 import styled from 'styled-components';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 const Carousel = () => {
   const [currentImg, setCurrentImg] = useState(0);
 
   return (
-    <CarFrame> 
-      <InnerFrame style={{backgroundImage: `url(${images[currentImg].img})`}}>
-        <Left onClick={() => {currentImg > 0 && setCurrentImg(currentImg - 1)}}>
-          <ArrowBackIosIcon style={{fontSize: 30 }}/>
-        </Left>
-        <Middle>
-          <h1>{images[currentImg].title}</h1>
-        </Middle>
-        <Right onClick={() => {currentImg < images.length - 1 && setCurrentImg(currentImg + 1)}}>
-          <ArrowForwardIosIcon style={{fontSize: 30 }}/>
-        </Right>
-      </InnerFrame>
-      <span><h4>{images[currentImg].location}</h4></span>
-    </CarFrame>
+    <>
+      <CarFrame> 
+        <InnerFrame style={{backgroundImage: `url(${images[currentImg].img})`}}>
+          <Left onClick={() => {currentImg > 0 && setCurrentImg(currentImg - 1)}}>
+            <ArrowBackIosIcon style={{fontSize: 30 }}/>
+          </Left>
+          <Middle>
+            <h1>{images[currentImg].title}</h1>
+          </Middle>
+          <Right onClick={() => {currentImg < images.length - 1 && setCurrentImg(currentImg + 1)}}>
+            <ArrowForwardIosIcon style={{fontSize: 30 }}/>
+          </Right>
+        </InnerFrame>
+        { /* eslint-disable-next-line react/jsx-no-target-blank */ }
+        <h4>{images[currentImg].location} <a href={images[currentImg].link} target="_blank" ><AddLocationAltIcon style={{cursor: 'pointer'}}/></a></h4>
+        <p>{images[currentImg].description}</p>
+      </CarFrame>
+    </>
   )
 }
 
 export default Carousel;
 
 const CarFrame = styled.div`
-  width: 90%;
+  width: 100%;
   height 700px;
   background: #ececec;
   color: antiquewhite;
+
+  h4 {
+    margin-left: 50px;
+  }
+
+  p {
+    margin-left: 50px;
+    margin-right: 50px;
+  }
   `;
 
 const InnerFrame = styled.div`
--- src/components/helpers/CarouselData.js
+++ src/components/helpers/CarouselData.js
@@ -6,16 +6,22 @@ export const images = [
   {
     title: "Garden of the Gods",
     location: "Colorado Springs, Colorado",
-    img: GOTG
+    description: "The Garden of the Gods Visitor and Nature Center is located at 1805 N. 30th Street and offers a view of the park. The center's information center and 30 educational exhibits are staffed by Parks, Recreation and Culture employees of the City of Colorado Springs. A short movie, How Did Those Red Rocks Get There?, runs every 20 minutes. A portion of the proceeds from the center's privately owned store and cafe support the non-profit Garden of the Gods Foundation; the money is used for maintenance and improvements to the park.[15] Natural history exhibits include minerals, geology, plants and local wildlife, as well as Native Americans who visited the park.[16] Programs include nature hikes and talks, a Junior Ranger program, narrated bus tours, movies, educational programs and special programs.[17]",
+    img: GOTG,
+    link: "https://www.google.com/maps/place/Garden+of+the+Gods/@38.8783536,-104.8719443,17z/data=!3m1!4b1!4m8!3m7!1s0x87135012b5cc9c93:0x4e9df5263bf6dab!5m2!4m1!1i2!8m2!3d38.8783536!4d-104.8697556"
   },
   {
     title: "Glacier National Park",
     location: "Glacier, Montana",
-    img: Glacier
+    description: "Glacier National Park is an American national park located in northwestern Montana, on the Canada–United States border, adjacent to the Canadian provinces of Alberta and British Columbia. The park encompasses more than 1 million acres (4,000 km2) and includes parts of two mountain ranges (sub-ranges of the Rocky Mountains), more than 130 named lakes, more than 1,000 different species of plants, and hundreds of species of animals. This vast pristine ecosystem is the centerpiece of what has been referred to as the Crown of the Continent Ecosystem, a region of protected land encompassing 16,000 square miles (41,000 km2).[4] The region that became Glacier National Park was first inhabited by Native Americans. Upon the arrival of European explorers, it was dominated by the Blackfeet in the east and the Flathead in the western regions. Under pressure, the Blackfeet ceded the mountainous parts of their treaty lands in 1895 to the federal government; it later became part of the park. Soon after the establishment of the park on May 11, 1910, a number of hotels and chalets were constructed by the Great Northern Railway. These historic hotels and chalets are listed as National Historic Landmarks and a total of 350 locations are on the National Register of Historic Places. By 1932 work was completed on the Going-to-the-Sun Road, later designated a National Historic Civil Engineering Landmark, which provided greater accessibility for automobiles into the heart of the park.",
+    img: Glacier,
+    link: "https://www.google.com/maps/place/Glacier+National+Park/@48.6587898,-114.4062192,9z/data=!3m1!4b1!4m5!3m4!1s0x5368901555555555:0xaf16bc2215c55dec!8m2!3d48.7596128!4d-113.7870225",
   },
   {
     title: "Guadalupe Peak",
     location: "Pine Spring, Texas",
+    description: "Guadalupe Peak, also known as Signal Peak,[3] is the highest natural point in Texas,[4] with an elevation of 8,751 feet (2,667 m) above sea level.[1] It is located in Guadalupe Mountains National Park, and is part of the Guadalupe Mountains range in southeastern New Mexico and West Texas. The mountain is about 90 miles (140 km) east of El Paso and about 50 miles (80 km) southwest of Carlsbad, New Mexico. The peak rises more than 3,000 feet (910 m) above the arid floor of the Chihuahuan Desert.",
     img: Guad,
+    link: "https://www.google.com/maps/place/Guadalupe+Peak/@31.8912253,-104.8780129,14z/data=!3m1!4b1!4m5!3m4!1s0x86e419cf03c71005:0x127f0b3453bf3dea!8m2!3d31.891227!4d-104.8605034"
   }
 ]
